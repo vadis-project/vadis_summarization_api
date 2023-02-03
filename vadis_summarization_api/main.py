d698e350008384d25001fbeaacc6c8e13eb52e30
sobamchan <oh.sore.sore.soutarou@gmail.com>
2023-02-03 13:55:03 +0100

    If input is in German, translate it to English first, then run summariation.

--- vadis_summarization_api/main.py
+++ vadis_summarization_api/main.py
@@ -1,10 +1,12 @@
 import os
 from typing import List, Union
 
+import langdetect
 from dotenv import load_dotenv
 from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers.pipelines import pipeline
 
 app = FastAPI()
 load_dotenv()
@@ -32,9 +34,9 @@ lang2lang_code = {
 
 
 class Model:
-    def __init__(self, lang: str = "en", use_gpu=False):
-        print(f"Initializing a model in {lang}...")
-        model_name = lang2model_name[lang]
+    def __init__(self, tgt_lang: str = "en", use_gpu=False):
+        print(f"Initializing a model in {tgt_lang}...")
+        model_name = lang2model_name[tgt_lang]
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
         bart = AutoModelForSeq2SeqLM.from_pretrained(model_name, use_auth_token=True)
@@ -44,15 +46,25 @@ class Model:
 
         self.use_gpu = use_gpu
         self.bart = bart
-        self.lang = lang
+        self.tgt_lang = tgt_lang
         self.model_name = model_name
+
+        self.translator = None
         print("loaded!")
 
     def summarize(self, text: str) -> str:
+        do_translation = True if langdetect.detect(text) == "de" else False
+        if do_translation:
+            print("German!!! Translate to English first.")
+            if self.translator is None:
+                self.translator = pipeline(model="facebook/wmt19-de-en")
+            text = self.translator([text])[0]["translation_text"]
+            print(text)
+
         inputs = self.tokenizer(
             [text], padding="max_length", truncation=True, return_tensors="pt"
         )
-        if self.lang == "en":
+        if self.tgt_lang == "en":
             summary_ids = self.bart.generate(
                 inputs["input_ids"].to("cuda" if self.use_gpu else "cpu"),
                 max_length=250,
@@ -66,7 +78,7 @@ class Model:
                 num_beams=1,
                 early_stopping=True,
                 decoder_start_token_id=self.tokenizer.lang_code_to_id[
-                    lang2lang_code[self.lang]
+                    lang2lang_code[self.tgt_lang]
                 ],
             )
         return self.tokenizer.batch_decode(summary_ids, skip_special_tokens=True)[0]
@@ -87,9 +99,17 @@ class Model:
 model = Model(os.environ.get("LANGUAGE", "en"))
 
 
+@app.post("/summarize_batch")
+def summarize_batch(texts: Texts, auth_key: Union[str, None] = Header(default=None)):
+    if os.environ["AUTH_KEY"] == auth_key:
+        return model.summarize_batch(texts.documents)
+    else:
+        return HTTPException(status_code=401, detail="Wrong key.")
+
+
 @app.post("/summarize")
 def summarize(texts: Texts, auth_key: Union[str, None] = Header(default=None)):
     if os.environ["AUTH_KEY"] == auth_key:
-        return model.summarize_batch(texts.documents)
+        return model.summarize(texts.documents[0])
     else:
         return HTTPException(status_code=401, detail="Wrong key.")
